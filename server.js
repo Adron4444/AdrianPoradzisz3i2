5f017554efb2c3db24ea92b3a220370e9fb1a790
adron4444 <adron4444@gmail.com>
2022-01-01 14:49:53 +0100

    Add: get /data

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
-app.get("/", function (req, res) {
-    res.send("<h1>Adrian Poradzisz 3i2a HerokuApp!</h1>")
-})
+const name = {imie: "Adrian", nazwisko: "Poradzisz", klasa: "3i2", grupa: "I/A"};
 
+app.get("/", function (req, res) {
+    res.send("<h1>Adrian Poradzisz 3i2a HerokuApp!</h1>");
+});
+
+app.get("/data", function (req, res) {
+    res.send(name);
+});
 app.listen(PORT, function () {
-    console.log("start serwera na porcie " + PORT)
-})
+    console.log("start serwera na porcie " + PORT);
+});
